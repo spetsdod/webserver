0fe88ded67c248dd9f31bbb89d5ab0343527a7b6
Sean Pendergrast <spetsdod@gmail.com>
2016-10-20 11:05:13 -0800

    Refactored middleware

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ var express = require('express');
 var app = express();
 const PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('Private route hit.');
-        next();
-    },
-    logger: function (req, res, next) {
-        var date = new Date().toString();
-        console.log('Request: '+date+' '+req.method+ ' ' +req.originalUrl);
-        next();
-    }
-};
+var middleware = require('./middleware.js');
+// var middleware = {
+//     requireAuthentication: function (req, res, next) {
+//         console.log('Private route hit.');
+//         next();
+//     },
+//     logger: function (req, res, next) {
+//         var date = new Date().toString();
+//         console.log('Request: '+date+' '+req.method+ ' ' +req.originalUrl);
+//         next();
+//     }
+// };
 
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
---
+++ middleware.js
@@ -0,0 +1,15 @@
+// module to expose middleware and logging
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('Private route hit.');
+        next();
+    },
+    logger: function (req, res, next) {
+        var date = new Date().toString();
+        console.log('Request: '+date+' '+req.method+ ' ' +req.originalUrl);
+        next();
+    }
+};
+
+module.exports = middleware;
