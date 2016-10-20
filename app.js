0afb79d55f499f16b7e7805170b74a600cb5ed72
Sean Pendergrast <spetsdod@gmail.com>
2016-10-20 10:45:35 -0800

    Initialize repo

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+const PORT = 3000;
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('Private route hit.');
+        next();
+    },
+    logger: function (req, res, next) {
+        var date = new Date().toString();
+        console.log('Request: '+date+' '+req.method+ ' ' +req.originalUrl);
+        next();
+    }
+};
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (request, response) {
+    response.send('About us.');
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.listen(PORT, function() {
+    console.log('Express server started on port: ' + PORT);
+});
